7f9a463ef82cdaa78313f2851ac95b55a6ae653e
João victor Souza Lemos <jolita.0312@gmail.com>
2022-09-01 19:50:24 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,17 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ContextProvider } from './contexts';
-
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import "./index.css";
 import App from "./App";
->>>>>>> e7e5ac672e18b06d4099451f6bc2df2967da437d
+import reportWebVitals from "./reportWebVitals";
+import { ContextProvider } from "./contexts";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <ContextProvider>
       <App />
     </ContextProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
